6b33acec0cf754d3be03bd8732ca11c397c5bf4f
Akash Jain <nahata.akash12@gmail.com>
2017-06-26 01:19:26 +0530

    Delete run_analysis.R

--- run_analysis.R
+++
@@ -1,3 +0,0 @@
-# 1. Downloading and unzipping
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(url = fileUrl, destfile = "Dataset.zip")
